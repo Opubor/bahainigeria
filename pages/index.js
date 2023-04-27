f352cecd27833040019fc7c718c3d86ac8fc5ced
Opubor Tony <opubortony@gmail.com>
2023-04-27 10:17:42 +0100

    Made some corrections

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
   const changeBackground = () => {
     console.log(window.scrollY)
-    if (window.scrollY >= 780) {
+    if (window.scrollY >= 550) {
       setBg(true)
     } else {
       setBg(false)
@@ -45,7 +45,6 @@ export default function Home() {
       >
         {/* ====navbar==== */}
         <nav className={`w-full bg-gray-800 shadow fixed z-50 bg-white ${bg ? "bg-white" : "bg-transparent"}`}>
-        {/* <nav className="w-full bg-gray-800 shadow fixed z-50 bg-white" style={{backgroundColor: "rgba(0, 0, 0, 0.316)"}}> */}
                 <div className="justify-between px-8 mx-auto xl:max-w-7xl xl:items-center xl:flex md:px-8">
                   <div>
                     <div className="flex items-center justify-between py-3 md:py-5 xl:block">
@@ -149,14 +148,13 @@ export default function Home() {
         {/* ====header-main==== */}
         <div className='flex justify-left xl:justify-between items-center gap-2 overflow-hidden'>
           <div className='h-screen pt-32 lg:pt-28 px-8 lg:pl-24 xl:px-44'>
-          {/* <div className='h-screen pl-8 pr-8 lg:pr-0 lg:pl-24 xl:pl-44 pt-32 lg:pt-48'> */}
               <div>
                 <h1 className='text-white text-2xl lg:text-4xl font-bold'>So powerful is the light <br></br> of unity that it can illuminate <br></br> the whole earth</h1>
                 <p className='text-white text-sm mb-6'>~Baha'u'llah~</p>
                 <a href='#teachings' className='py-2 px-2 lg:px-8 bg-white rounded-lg hover:text-blue-900 text-sm xl:text-lg lg:font-semibold hover:bg-blue-200'>Baha`i` Teachings</a>
                 
-                <p className='text-white text-sm xl:text-lg mt-8 lg:mt-24'>There is one God; mankind is one; the foundations of religion is one. <br></br>Let us worship him and give praise for all his great prophets and messengers who have <br></br> manifested his brightness and Glory</p>
-                <p className='text-white mb-4'>~Abdul-baha~</p>
+                <p className='text-white text-sm xl:text-lg mt-6 lg:mt-24'>There is one God; mankind is one; the foundations of religion is one. <br></br>Let us worship him and give praise for all his great prophets and messengers who have <br></br> manifested his brightness and Glory</p>
+                <p className='text-white text-sm mb-4'>~Abdul-baha~</p>
               </div>
               <div>
 
@@ -271,7 +269,7 @@ export default function Home() {
       </div>
 
       {/* ====baha`i`-writings==== */}
-      <div id='teachings' className='bg-blue-50 px-8 lg:px-24 xl:px-44 py-12'
+      <div id='teachings' className='px-8 lg:px-24 xl:px-44 py-12'
         style={{
           backgroundImage: 'url(/aa.svg)',
           backgroundSize: "cover",
@@ -280,8 +278,8 @@ export default function Home() {
         >
         <h1 className="font-bold text-2xl lg:text-4xl text-center">BAHA`I` WRITINGS</h1>
         <div className='flex gap-4 mt-4'>
-          <p className='font-semibold'>Tags: </p>
-          <div className='hidden lg:grid grid-cols-4 lg:grid-cols-7 gap-2 w-full'>
+          {/* <p className='font-semibold'>Tags: </p> */}
+          {/* <div className='hidden lg:grid grid-cols-4 lg:grid-cols-7 gap-2 w-full'>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Unity</p>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Forgiveness</p>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Faith</p>
@@ -289,16 +287,16 @@ export default function Home() {
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Marrriage</p>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Justice</p>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Education</p>
-          </div>
+          </div> */}
         </div>
-        <div className='hidden lg:grid grid-cols-4 lg:grid-cols-6 gap-2 mt-2 lg:px-32 w-full'>
+        {/* <div className='hidden lg:grid grid-cols-4 lg:grid-cols-6 gap-2 mt-2 lg:px-32 w-full'>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Service</p>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Youth</p>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Consultation</p>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Humility</p>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>History</p>
               <p className='text-center bg-white shadow-lg shadow-slate-300 hover:bg-blue-900 hover:text-white duration-300'>Patience</p>
-        </div>
+        </div> */}
 
         <div className='grid grid-cols-1 lg:grid-cols-3 mt-6 gap-4'>
           <div className='bg-white shadow-lg shadow-slate-300 lg:h-auto p-8 border-8 border-dotted border-yellow-200 duration-300'>
@@ -352,7 +350,7 @@ export default function Home() {
 
       
       {/* ===bahai-prayers=== */}
-      <div id='prayers' className='bg-blue-50 px-8 lg:px-24 xl:px-44 py-12'
+      <div id='prayers' className='bg-blue-50 px-8 lg:px-24 xl:px-44 py-12 border-t-2 border-t-blue-900 lg:border-t-0'
         style={{
           backgroundImage: 'url(/dd.svg)',
           backgroundSize: "cover",
@@ -719,7 +717,7 @@ export default function Home() {
           <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-24">
               <div className="md:flex md:justify-between">
                 <div className="mb-6 md:mb-0">
-                    <a href="https://flowbite.com/" className="flex items-center gap-2">
+                    <a href="/" className="flex items-center gap-2">
                     <Image
                       src="/PngItem_4219473.png"
                       alt="Vercel Logo"
@@ -737,10 +735,10 @@ export default function Home() {
                         <h2 className="mb-6 text-sm font-semibold text-gray-200 uppercase">More</h2>
                         <ul className="text-gray-600 dark:text-gray-400 font-medium">
                             <li className="mb-4">
-                                <a href="https://flowbite.com/" className="hover:underline">Baha`i` Teachings</a>
+                                <a href="#teachings" className="hover:underline">Baha`i` Teachings</a>
                             </li>
                             <li>
-                                <a href="https://tailwindcss.com/" className="hover:underline">Baha`i` Prayers</a>
+                                <a href="#prayers" className="hover:underline">Baha`i` Prayers</a>
                             </li>
                         </ul>
                     </div>
@@ -748,10 +746,10 @@ export default function Home() {
                         <h2 className="mb-6 text-sm font-semibold text-gray-200 uppercase dark:text-white">Follow us</h2>
                         <ul className="text-gray-600 dark:text-gray-400 font-medium">
                             <li className="mb-4">
-                                <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Facebook</a>
+                                <a href="#" className="hover:underline ">Facebook</a>
                             </li>
                             <li>
-                                <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Instagram</a>
+                                <a href="#" className="hover:underline">Instagram</a>
                             </li>
                         </ul>
                     </div>
@@ -770,7 +768,7 @@ export default function Home() {
             </div>
             <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <div className="sm:flex sm:items-center sm:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Baháʼí Faith™</a>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">Baháʼí Faith™</a>. All Rights Reserved.
                 </span>
                 <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                     <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
@@ -781,18 +779,18 @@ export default function Home() {
                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clipRule="evenodd" /></svg>
                         <span className="sr-only">Instagram page</span>
                     </a>
-                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                    {/* <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
                         <span className="sr-only">Twitter page</span>
-                    </a>
-                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                    </a> */}
+                    {/* <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
                         <span className="sr-only">GitHub account</span>
-                    </a>
-                    <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                    </a> */}
+                    {/* <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z" clipRule="evenodd" /></svg>
                         <span className="sr-only">Dribbble account</span>
-                    </a>
+                    </a> */}
                 </div>
             </div>
           </div>
